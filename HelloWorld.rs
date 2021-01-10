02f2bdf9cd22b2309f4612692fb43e84661f8578
richrosenthal <rrosenthal2013@gmail.com>
2021-01-10 16:45:47 -0500

    create HelloWorld.rs

---
+++ HelloWorld.rs
@@ -0,0 +1,9 @@
+//Richard Rosenthal
+//Date 1-10-21
+//LocalHackDay Day One Challenge: Hello World in a new language.
+fn main() {
+
+    println!("Hello World!");
+    println!("My Name is Richard");
+    println!("This is the first time I've coded in Rust"); 
+}
